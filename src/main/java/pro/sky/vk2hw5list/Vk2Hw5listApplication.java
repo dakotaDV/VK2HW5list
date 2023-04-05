059e0fd5dd8462207bbd1dac6474ba7177b8167a
Prudnikova <dakoto4ka@yandex.ru>
2023-04-05 15:27:31 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/vk2hw5list/Vk2Hw5listApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.vk2hw5list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vk2Hw5listApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vk2Hw5listApplication.class, args);
+    }
+
+}
